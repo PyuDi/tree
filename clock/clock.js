248d42e5b80cec938f88ab26b62fee8413d4f9ea
pyu_ <36254894+is-pyu@users.noreply.github.com>
2021-01-05 14:05:01 +0900

    [clock] fix code indent

--- clock/clock.js
+++ clock/clock.js
@@ -1,17 +1,17 @@
 function fz(obj, len) {
-  obj='000000000000000'+obj;
-  return obj.substring(obj.length-len);
+	obj='000000000000000'+obj;
+	return obj.substring(obj.length-len);
 } 
 var type=1;
 var clock = document.querySelector('.clock');
 function getTime(){
-  window.time = new Date();
+	window.time = new Date();
 	window.Y = time.getFullYear();
 	window.M = time.getMonth()+1;
 	window.D = time.getDate();
-  window.h = time.getHours();
-  window.m = time.getMinutes();
-  window.s = time.getSeconds();
+	window.h = time.getHours();
+	window.m = time.getMinutes();
+	window.s = time.getSeconds();
 	if (type==1) {
 		clock.innerHTML = fz(Y,4) + "-" + fz(M,2) + "-" + fz(D,2) + " " + fz(h,2) + ":" + fz(m,2) + ":" + fz(s,2);
 	} else if (type==11) {
@@ -49,12 +49,12 @@ function f21() {
 	type=21;
 }
 function hide() {
-  var x = document.getElementById("table");
-  if (x.style.visibility !== "hidden") {
-    x.style.visibility = "hidden";
+	var x = document.getElementById("table");
+	if (x.style.visibility !== "hidden") {
+		x.style.visibility = "hidden";
 		document.getElementById("button").innerHTML = "Show Selections";
-  } else {
-    x.style.visibility = "visible";
+	} else {
+		x.style.visibility = "visible";
 		document.getElementById("button").innerHTML = "Hide Selections";
-  }
+	}
 }
